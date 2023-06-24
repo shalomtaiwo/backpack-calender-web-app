8d2e8eea5cd03d9d665ca14224aff07f7fe3b5a3
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2023-06-24 21:17:05 +0200

    Using Backpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import 'bpk-stylesheets/base';
-import 'bpk-stylesheets/base.css';
-import 'bpk-stylesheets/font';
-import 'bpk-stylesheets/font.css';
+import "bpk-stylesheets/base";
+import "bpk-stylesheets/base.css";
+import "bpk-stylesheets/font";
+import "bpk-stylesheets/font.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
